1e490c1bd1225e1273222e74da15e4d5008a2a55
Dewan Mahabub Sarwar <sarwarcse@gmail.com>
2015-11-09 17:09:20 +1300

    Integrated nodejs steiner algorithm with the standalone d3 program, but need to fix to make it working.

--- index.js
+++ index.js
@@ -1,17 +1,154 @@
 /**
  * Created by Dewan on 11/1/2015.
  */
+
+var d3 = require('d3');
+var steiner = require('./steiner.js').appx_steiner;
+
 var nodes = [];
 var edges = [];
-//var reqds = ['nucleus dorsolateralis anterior thalami pars lateralis',
-//    'abducens nucleus',
-//    'parietal area pg  medial part',
-//    'medial nucleus of the amygdala posterodorsal part sublayer c',
-//    'visual area v3',
-//    'middle temporal cortex (occipital)',
-//    'agranular area of temporal polar cortex',
-//    'intergeniculate leaflet of the lateral geniculate complex'];
 var reqds = [];
+
+function reverse_edges(edges) {
+    return edges.map(function(e) {
+        return [e[1],e[0],e[2]];
+    });
+}
+
+function test(a,b,c) {
+    b = b.map(function(e) {
+        //console.log({from:e[0], to:e[1], weight:e[2]});
+        return {from:e[0], to:e[1], weight:e[2]};
+    });
+
+    reverse_edges = b.map(function (e) {
+        return {from: e.to, to: e.from, weight: e.weight, species: e.species};
+    });
+
+    b = b.concat(reverse_edges);
+
+    var result = steiner(a,b,c);
+    console.log("\nEdges:");
+    var counter = 0;
+    result = result.map(function(e) {
+        counter++;
+        //console.log([e.from, e.to, e.weight]);
+        return [e.from, e.to, e.weight];
+    });
+
+    console.log("\nNumber of Edges: " + counter);
+
+    var got = JSON.stringify(result);
+    console.log("\nOUTPUT:\n" + got);
+
+    return got;
+}
+
+function draw(result) {
+    var links = [];
+    for (var i = 0; i < result.length; i++) {
+        links.push({
+            source: result[i][0],
+            target: result[i][1],
+            weight: result[i][2]
+        });
+    }
+
+    var nodes = {};
+
+    // Compute the distinct nodes from the links.
+    links.forEach(function (link) {
+        link.source = nodes[link.source] ||
+            (nodes[link.source] = {name: link.source});
+        //console.log(link.source);
+
+        link.target = nodes[link.target] ||
+            (nodes[link.target] = {name: link.target});
+        //console.log(link.target);
+
+        link.weight = +link.weight;
+        //console.log(link.weight);
+    });
+
+    var width = 1200,
+        height = 700;
+
+    var color = d3.scale.category20();
+
+    var force = d3.layout.force()
+        .nodes(d3.values(nodes))
+        .links(links)
+        .size([width, height])
+        .linkDistance(60)
+        .charge(-800)
+        .on("tick", tick)
+        .start();
+
+    var svg = d3.select("#graph").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    // build the arrow.
+    svg.append("svg:defs").selectAll("marker")
+        .data(["end"])      // Different link/path types can be defined here
+        .enter().append("svg:marker")    // This section adds in the arrows
+        .attr("id", String)
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 15)
+        .attr("refY", -1.5)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 6)
+        .attr("orient", "auto")
+        .append("svg:path")
+        .attr("d", "M0,-5L10,0L0,5");
+
+    // add the links and the arrows
+    var path = svg.append("svg:g").selectAll("path")
+        .data(force.links())
+        .enter().append("svg:path")
+        .attr("class", "link")
+        .style("stroke-width", function(d) { return Math.sqrt(d.value); })
+        .attr("marker-end", "url(#end)");
+
+    // define the nodes
+    var node = svg.selectAll(".node")
+        .data(force.nodes())
+        .enter().append("g")
+        .attr("class", "node")
+        .style("fill", function(d) { return color(d.group); })
+        .call(force.drag);
+
+    // add the nodes
+    node.append("circle")
+        .attr("r", 5);
+
+    // add the text
+    node.append("text")
+        .attr("x", 12)
+        .attr("dy", ".35em")
+        .text(function (d) {
+            return d.name;
+        });
+
+    // add the curvy lines
+    function tick() {
+        path.attr("d", function (d) {
+            var dx = d.target.x - d.source.x,
+                dy = d.target.y - d.source.y,
+                dr = Math.sqrt(dx * dx + dy * dy);
+            return "M" +
+                d.source.x + "," +
+                d.source.y + "A" +
+                dr + "," + dr + " 0 0,1 " +
+                d.target.x + "," +
+                d.target.y;
+        });
+
+        node.attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+    }
+}
 
 d3.json("data.json", function (data) {
     var brainList = document.getElementById("brainlist");
@@ -31,17 +168,12 @@ d3.json("data.json", function (data) {
         }
 
         var button = document.getElementById("calculate");
-        //var graph = document.getElementById("graph");
         button.onclick = function () {
-            draw();
+            var result = test(nodes,edges,reqds);
+            draw(result);
             console.log("REQUIRED NODES:\n" + reqds);
             console.log("\nNODES:\n" + nodes);
             console.log("\nEDGES:\n" + edges);
-
-            //for (i = 0; i < reqds.length; i++) {
-            //    graph.appendChild(document.createTextNode(reqds[i]));
-            //    graph.appendChild(document.createElement("br"));
-            //}
         }
 
         brainList.appendChild(checkbox);
@@ -57,128 +189,3 @@ d3.json("data.json", function (data) {
         }
     }
 })
-
-function draw() {
-
-    var output = [
-        ["parietal area pg  medial part", "area 7", 2],
-        ["area 7", "area 23", 2],
-        ["middle temporal cortex (occipital)", "lateral geniculate body", 1],
-        ["lateral geniculate body", "visual area 1", 1],
-        ["visual area 1", "visual area 2", 1],
-        ["visual area 2", "area 23", 2],
-        ["intergeniculate leaflet of the lateral geniculate complex", "precommissural nucleus", 5],
-        ["medial nucleus of the amygdala posterodorsal part sublayer c", "precommissural nucleus", 5],
-        ["agranular area of temporal polar cortex", "orbitofrontal area 13", 2],
-        ["orbitofrontal area 13", "nucleus pulvinaris lateralis thalami", 2],
-        ["visual area 1", "nucleus pulvinaris lateralis thalami", 2],
-        ["nucleus dorsolateralis anterior thalami pars lateralis", "habenula", 7],
-        ["habenula", "pallium", 7],
-        ["pallium", "hippocampus", 7],
-        ["hippocampus", "nucleus medialis dorsalis thalami", 2],
-        ["orbitofrontal area 13", "nucleus medialis dorsalis thalami", 2],
-        ["nucleus of the posterior commissure", "precommissural nucleus", 5],
-        ["nucleus pulvinaris lateralis thalami", "nucleus of the posterior commissure", 2]
-    ]
-
-    var links = [];
-    for (var i = 0; i < output.length; i++) {
-        links.push({
-            source: output[i][0],
-            target: output[i][1],
-            weight: output[i][2]
-        });
-    }
-
-    var nodes = {};
-
-// Compute the distinct nodes from the links.
-    links.forEach(function (link) {
-        link.source = nodes[link.source] ||
-            (nodes[link.source] = {name: link.source});
-
-        //console.log(link.source);
-        link.target = nodes[link.target] ||
-            (nodes[link.target] = {name: link.target});
-        //console.log(link.target);
-    });
-
-    var width = 1200,
-        height = 700;
-
-    //console.log(d3.values(nodes));
-    //console.log(links);
-
-    var force = d3.layout.force()
-        .nodes(d3.values(nodes))
-        .links(links)
-        .size([width, height])
-        .linkDistance(60)
-        .charge(-800)
-        .on("tick", tick)
-        .start();
-
-    var svg = d3.select("#graph").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-// build the arrow.
-    svg.append("svg:defs").selectAll("marker")
-        .data(["end"])      // Different link/path types can be defined here
-        .enter().append("svg:marker")    // This section adds in the arrows
-        .attr("id", String)
-        .attr("viewBox", "0 -5 10 10")
-        .attr("refX", 15)
-        .attr("refY", -1.5)
-        .attr("markerWidth", 6)
-        .attr("markerHeight", 6)
-        .attr("orient", "auto")
-        .append("svg:path")
-        .attr("d", "M0,-5L10,0L0,5");
-
-// add the links and the arrows
-    var path = svg.append("svg:g").selectAll("path")
-        .data(force.links())
-        .enter().append("svg:path")
-        //    .attr("class", function(d) { return "link " + d.type; })
-        .attr("class", "link")
-        .attr("marker-end", "url(#end)");
-
-// define the nodes
-    var node = svg.selectAll(".node")
-        .data(force.nodes())
-        .enter().append("g")
-        .attr("class", "node")
-        .call(force.drag);
-
-// add the nodes
-    node.append("circle")
-        .attr("r", 5);
-
-// add the text
-    node.append("text")
-        .attr("x", 12)
-        .attr("dy", ".35em")
-        .text(function (d) {
-            return d.name;
-        });
-
-// add the curvy lines
-    function tick() {
-        path.attr("d", function (d) {
-            var dx = d.target.x - d.source.x,
-                dy = d.target.y - d.source.y,
-                dr = Math.sqrt(dx * dx + dy * dy);
-            return "M" +
-                d.source.x + "," +
-                d.source.y + "A" +
-                dr + "," + dr + " 0 0,1 " +
-                d.target.x + "," +
-                d.target.y;
-        });
-
-        node.attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
-        });
-    }
-}
