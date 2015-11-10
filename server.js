6ff551f6c26b50cac7f0b7f69300351649ecb25c
Dewan Mahabub Sarwar <sarwarcse@gmail.com>
2015-11-11 12:54:29 +1300

    added client side files in public directory

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ var server = app.listen(8081, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log("Server is listening at http://%s:%s", host.toString(), port.toString());
+    console.log("Server is listening at http://%s:%s", host, port);
 })
